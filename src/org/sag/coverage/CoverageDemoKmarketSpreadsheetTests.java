f22b93081ccc1f3bc77b831e4765200683ef9778
pengshuai2010 <pengshuai2010@gmail.com>
2015-10-18 22:35:16 -0600

    updated CoverageDemoKmarketSpreadsheetTests class

--- src/org/sag/coverage/CoverageDemoKmarketSpreadsheetTests.java
+++ src/org/sag/coverage/CoverageDemoKmarketSpreadsheetTests.java
@@ -5,7 +5,11 @@ import org.sag.faultlocalization.SpectrumBasedFaultLocalizer;
 
 public class CoverageDemoKmarketSpreadsheetTests {
 	public static void main(String[] args) throws Exception{
-		PolicySpreadSheetTestSuite testSuite = new PolicySpreadSheetTestSuite("tests//kmarket-tests.xls","tests//kmarket-blue-policy.xml");
+		//PolicySpreadSheetTestSuite testSuite = new PolicySpreadSheetTestSuite("tests//kmarket-tests.xls","tests//kmarket-blue-policy.xml");
+		String policyFileToRepair = "Experiments//conference3//mutants//conference3_ANR4.xml";
+		String testSuiteSpreadSheetFile = "Experiments//conference3//test_suites//conference3_MCDCCoverage_NoError//conference3_MCDCCoverage_NoError.xls";
+		PolicySpreadSheetTestSuite testSuite = new PolicySpreadSheetTestSuite(testSuiteSpreadSheetFile,
+				policyFileToRepair);
 		testSuite.runAllTests();
 		System.out.println("\n");
 		testSuite.generateJUnitFile("src", 
