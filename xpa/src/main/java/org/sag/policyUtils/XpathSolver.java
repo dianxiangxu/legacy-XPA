3f31ec84b21002c6b659859b65c4163cd81a17fc
shuaipeng <pengshuai2010@gmail.com>
2016-10-25 17:53:43 -0600

    add support for relative xpath

--- xpa/src/main/java/org/sag/policyUtils/XpathSolver.java
+++ xpa/src/main/java/org/sag/policyUtils/XpathSolver.java
@@ -1,11 +1,13 @@
 package org.sag.policyUtils;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.wso2.balana.DOMHelper;
+import org.wso2.balana.*;
 
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
@@ -23,6 +25,7 @@ import java.util.regex.Pattern;
  * Created by shuaipeng on 10/20/16.
  */
 public class XpathSolver {
+    private static Log logger = LogFactory.getLog(XpathSolver.class);
     private static Pattern policyPattern = Pattern.compile("(?:\\w+:)*Policy");
     private static Pattern policysetPattern = Pattern.compile("(?:\\w+:)*PolicySet");
     private static Pattern rulePattern = Pattern.compile("(?:\\w+:)*Rule");
@@ -74,7 +77,7 @@ public class XpathSolver {
         }
     }
 
-    private static void buildEntryList(List<String> entries, String parentXPath, Element parent) {
+    private static void buildEntryListAbsoluteXPath(List<String> entries, String parentXPath, Element parent) {
         String name = DOMHelper.getLocalName(parent);
         //here we use regex match instead of simply String.equals() because some XACML may use namespace, in such case
         // the local name is "namespace:Policy" instead of "Policy".
@@ -88,27 +91,113 @@ public class XpathSolver {
                 Node child = children.item(i);
                 if (child instanceof Element) {
                     String identifier = getNodeIdentifier(child);
-                    buildEntryList(entries, parentXPath + "/" + identifier, (Element) child);
+                    buildEntryListAbsoluteXPath(entries, parentXPath + "/" + identifier, (Element) child);
                 }
             }
         }
     }
 
-    public static List<String> getEntryList(Document doc) {
+    /**
+     * get the absolute xpathes of all rule and policy target and policyset target elements.
+     *
+     * @param doc
+     * @return a list of absolute xpathes
+     */
+    static List<String> getEntryListAbsoluteXPath(Document doc) {
         ArrayList<String> entries = new ArrayList<>();
         Element root = doc.getDocumentElement();
-        buildEntryList(entries, "/" + getNodeIdentifier(root), root);
+        buildEntryListAbsoluteXPath(entries, "/" + getNodeIdentifier(root), root);
         return entries;
     }
 
-    public static String buildXpath(Node node) {
+    private static void buildEntryListRelativeXPath(List<String> entries, Element node) {
+        String name = DOMHelper.getLocalName(node);
+        //here we use regex match instead of simply String.equals() because some XACML may use namespace, in such case
+        // the local name is "namespace:Policy" instead of "Policy".
+        if (rulePattern.matcher(name).matches()) {
+            entries.add("//" + getNodeIdentifier(node));
+        } else if (targetPattern.matcher(name).matches() && !rulePattern.matcher(DOMHelper.getLocalName(node.getParentNode())).matches()) {
+            entries.add("//" + getNodeIdentifier(node.getParentNode()) + "/" + getNodeIdentifier(node));
+        }
+
+        if (policyPattern.matcher(name).matches() || policysetPattern.matcher(name).matches()) {
+            NodeList children = node.getChildNodes();
+            for (int i = 0; i < children.getLength(); i++) {
+                Node child = children.item(i);
+                if (child instanceof Element) {
+                    buildEntryListRelativeXPath(entries, (Element) child);
+                }
+            }
+        }
+    }
+
+    /**
+     * get the relative xpathes of all rule, policy target and policyset target elements
+     *
+     * @param doc
+     * @return a list of relative xpathes
+     */
+    public static List<String> getEntryListRelativeXPath(Document doc) {
+        ArrayList<String> entries = new ArrayList<>();
+        Element root = doc.getDocumentElement();
+        buildEntryListRelativeXPath(entries, root);
+        return entries;
+    }
+
+    /**
+     * given a node, get the absolute xpath of this node by recursively tracing back its ancestors
+     *
+     * @param node
+     * @return
+     */
+    static String buildNodeXpath(Node node) {
         Node parent = node.getParentNode();
         String parentXpath = "";
         if (parent != null && parent.getParentNode() != null)
-            parentXpath = buildXpath(parent);
+            parentXpath = buildNodeXpath(parent);
         return parentXpath + "/" + getNodeIdentifier(node);
     }
 
+    /**
+     * get the relative xpath of a <code>Rule</code>.
+     * <p>
+     * Note that it's impossible to get absolute xpath of any balana elements, including <code>Policy</code> ,
+     * <code>Policyset</code>, <code>Rule</code>, <code>Target</code>, because there's no getParent() method in those classes.
+     *
+     * @param rule
+     * @return
+     */
+    public static String buildRuleXpath(Rule rule) {
+        return String.format("//%s[@%s='%s']", "Rule", "RuleId", rule.getId());
+    }
+
+    /**
+     * get the relative xpath of a policy target or a policyset target
+     *
+     * @param policy
+     * @return
+     */
+    public static String buildTargetXpath(AbstractPolicy policy) {
+        String policyString;
+        if (policy instanceof Policy)
+            policyString = "Policy";
+        else if (policy instanceof PolicySet)
+            policyString = "PolicySet";
+        else
+            throw new RuntimeException(policy.getId().toString() + " is neither Policy or PolicySet");
+        return String.format("//%s[@%s='%s']/Target[1]", policyString, policyString + "Id", policy.getId().toString());
+    }
+
+    /**
+     * get the identifier of a node in an XACML document, e.g. "Rule[@RuleId='rule13']". A target doesn't have an id
+     * attribute, so simply return a "Target[1]".
+     * <p>
+     * Note that for policy, policyset and rule, the uniqueness of the identifier replies on the uniqueness of their id
+     * attribute.
+     *
+     * @param node a node in an XACML document
+     * @return
+     */
     private static String getNodeIdentifier(Node node) {
         String nodeName = node.getNodeName();
         String idAttr = "";
