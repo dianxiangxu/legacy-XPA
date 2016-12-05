476723b75e7bff0d2199e5ac4890e2f43711999e
shuaipeng <pengshuai2010@gmail.com>
2016-12-05 16:45:27 -0700

    add two mutation methods and their tests

--- xpa/src/main/java/org/sag/mutation/Mutator.java
+++ xpa/src/main/java/org/sag/mutation/Mutator.java
@@ -1,25 +1,137 @@
 package org.sag.mutation;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.sag.policyUtils.PolicyLoader;
+import org.sag.policyUtils.XpathSolver;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.wso2.balana.AbstractPolicy;
+import org.wso2.balana.ParsingException;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.*;
+
 /**
  * Created by shuaipeng on 9/8/16.
  */
 public class Mutator {
+    private static Log logger = LogFactory.getLog(Mutator.class);
     // so far only string and integer are considered.
-    String int_function = "urn:oasis:names:tc:xacml:1.0:function:integer-equal";
-    String str_function = "urn:oasis:names:tc:xacml:1.0:function:string-equal";
-    String int_function_one_and_only = "urn:oasis:names:tc:xacml:1.0:function:integer-one-and-only";
-    String str_function_one_and_only = "urn:oasis:names:tc:xacml:1.0:function:string-one-and-only";
-    String str_value = "RANDOM$@^$%#&!";
-    String str_value1 = "str_A";
-    String str_value2 = "str_B";
-    String int_value = "-98274365923795632";
-    String int_value1 = "123456789";
-    String int_value2 = "-987654321";
+    private String int_function = "urn:oasis:names:tc:xacml:1.0:function:integer-equal";
+    private String str_function = "urn:oasis:names:tc:xacml:1.0:function:string-equal";
+    private String int_function_one_and_only = "urn:oasis:names:tc:xacml:1.0:function:integer-one-and-only";
+    private String str_function_one_and_only = "urn:oasis:names:tc:xacml:1.0:function:string-one-and-only";
+    private String str_value = "RANDOM$@^$%#&!";
+    private String str_value1 = "str_A";
+    private String str_value2 = "str_B";
+    private String int_value = "-98274365923795632";
+    private String int_value1 = "123456789";
+    private String int_value2 = "-987654321";
     private Mutant baseMutant;
+    private List<String> xpathList;
+    private Map<String, Integer> xpathMapping;
+    private Document doc;
+    private XPath xPath;
 
-    public Mutator(Mutant baseMutant) {
+    public Mutator(Mutant baseMutant) throws ParserConfigurationException, SAXException, IOException {
         this.baseMutant = baseMutant;
+        InputStream stream = IOUtils.toInputStream(baseMutant.encode(), Charset.defaultCharset());
+        doc = PolicyLoader.getDocument(stream);
+        xpathList = XpathSolver.getEntryListRelativeXPath(doc);
+        xpathMapping = new HashMap<>();
+        for (int i = 0; i < xpathList.size(); i++) {
+            xpathMapping.put(xpathList.get(i), i);
+        }
+        xPath = XPathFactory.newInstance().newXPath();
+    }
 
+    static boolean isEmptyTarget(Node node) {
+        // When the target is empty, it may have one child node that contains only text "\n"; when the target is not empty,
+        // it may have three nodes: "\n", AnyOf element and "\n".
+        NodeList children = node.getChildNodes();
+        boolean isEmptyTarget = true;
+        for (int i = 0; i < children.getLength(); i++) {
+            // we don't use "if (child.getNodeName().equals("AnyOf"))" here for backward compatibility with XACML 2.0.
+            if (!children.item(i).getNodeName().equals("#text")) {
+                isEmptyTarget = false;
+            }
+        }
+        return isEmptyTarget;
+    }
+
+    public static void main(String[] args) throws ParserConfigurationException, ParsingException, SAXException, IOException, XPathExpressionException {
+        File file = new File("src/test/resources/org/sag/policies/HL7/HL7.xml");
+        String xpathString = "//*[local-name()='PolicySet' and @PolicySetId='http://axiomatics.com/alfa/identifier/com.axiomatics.hl7.global.medicalRecords']/*[local-name()='Target' and 1]";
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        AbstractPolicy policy = PolicyLoader.loadPolicy(file);
+        Document doc = PolicyLoader.getDocument(IOUtils.toInputStream(policy.encode(), Charset.defaultCharset()));
+        NodeList nodes = (NodeList) xPath.evaluate(xpathString, doc.getDocumentElement(), XPathConstants.NODESET);
+        // assert only one
+        Node node = nodes.item(0);
+        System.out.println(XpathSolver.NodeToString(node, false, true));
+        Node child = node.getFirstChild();
+        node.removeChild(child);
+        System.out.println(XpathSolver.NodeToString(node, false, true));
+        node.appendChild(child);
+        System.out.println(XpathSolver.NodeToString(node, false, true));
+
+    }
+
+    public List<Mutant> createRuleEffectFlippingMutants(String xpathString) throws XPathExpressionException, ParsingException {
+        List<Mutant> list = new ArrayList<>();
+        NodeList nodes = (NodeList) xPath.evaluate(xpathString, doc.getDocumentElement(), XPathConstants.NODESET);
+        Node node = nodes.item(0);
+        //change doc
+        if (node.getAttributes().getNamedItem("Effect").getTextContent().equals("Deny")) {
+            node.getAttributes().getNamedItem("Effect").setTextContent("Permit");
+        } else {
+            node.getAttributes().getNamedItem("Effect").setTextContent("Deny");
+        }
+        AbstractPolicy newPolicy = PolicyLoader.loadPolicy(doc);
+        //restore doc
+        if (node.getAttributes().getNamedItem("Effect").getTextContent().equals("Deny")) {
+            node.getAttributes().getNamedItem("Effect").setTextContent("Permit");
+        } else {
+            node.getAttributes().getNamedItem("Effect").setTextContent("Deny");
+        }
+        int faultLocation = xpathMapping.get(xpathString);
+        list.add(new Mutant(newPolicy, Collections.singletonList(faultLocation), "CRE" + faultLocation));
+        return list;
+    }
+
+    public List<Mutant> createPolicyTargetTrueMutants(String xpathString) throws XPathExpressionException, ParsingException, IOException, ParserConfigurationException, SAXException {
+        List<Mutant> list = new ArrayList<>();
+        NodeList nodes = (NodeList) xPath.evaluate(xpathString, doc.getDocumentElement(), XPathConstants.NODESET);
+        Node node = nodes.item(0);
+        if (!isEmptyTarget(node)) {
+            //change doc
+            List<Node> children = new ArrayList<>();
+            while (node.hasChildNodes()) {
+                Node child = node.getFirstChild();
+                children.add(child);
+                node.removeChild(child);
+            }
+            AbstractPolicy newPolicy = PolicyLoader.loadPolicy(doc);
+            int faultLocation = xpathMapping.get(xpathString);
+            list.add(new Mutant(newPolicy, Collections.singletonList(faultLocation), "PTT" + faultLocation));
+            //restore doc
+            for (Node child : children) {
+                node.appendChild(child);
+            }
+        }
+        return list;
     }
 
 }
---
+++ xpa/src/test/java/org/sag/mutation/MutatorTest.java
@@ -0,0 +1,128 @@
+package org.sag.mutation;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.sag.policyUtils.PolicyLoader;
+import org.sag.policyUtils.ReflectionUtils;
+import org.sag.policyUtils.XpathSolver;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.wso2.balana.AbstractPolicy;
+import org.wso2.balana.ParsingException;
+import org.xml.sax.SAXException;
+import org.xmlunit.builder.DiffBuilder;
+import org.xmlunit.diff.ComparisonControllers;
+import org.xmlunit.diff.Diff;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.List;
+
+/**
+ * Created by shuaipeng on 12/1/16.
+ */
+public class MutatorTest {
+    private XPath xPath = XPathFactory.newInstance().newXPath();
+    private List<String> xpathList;
+    private Mutator mutator;
+    private Document doc;
+
+    private boolean isRuleXpathString(String xPathString) {
+        return xPathString.contains("[local-name()='Rule'");
+    }
+
+    private boolean isTargetXpathString(String xPathString) {
+        return xPathString.contains("[local-name()='Target'");
+    }
+
+    @Before
+    public void initialize() throws ParserConfigurationException, ParsingException, SAXException, IOException {
+        File file = new File("src/test/resources/org/sag/policies/HL7/HL7.xml");
+        AbstractPolicy policy = PolicyLoader.loadPolicy(file);
+        doc = PolicyLoader.getDocument(IOUtils.toInputStream(policy.encode(), Charset.defaultCharset()));
+        xpathList = XpathSolver.getEntryListRelativeXPath(doc);
+        mutator = new Mutator(new Mutant(policy, ""));
+    }
+
+    @After
+    public void checkIfDocIsStored() {
+        // make sure the doc in Mutator is properly restored
+        // compiler warnings like "" will appear, which is caused by a bug in JVM
+        // there's little we can do about this
+        // see http://stackoverflow.com/questions/25453042/how-to-disable-accessexternaldtd-and-entityexpansionlimit-warnings-with-logback
+        isSameDoc(doc, (Document) ReflectionUtils.getField(mutator, "doc"));
+    }
+
+    private void isSameDoc(Document docBefore, Document docAfter) {
+        Diff diff = DiffBuilder.compare(docBefore)
+                .withTest(docAfter)
+                .checkForSimilar()
+                .ignoreComments()
+                .ignoreWhitespace()
+                .normalizeWhitespace()
+                .withComparisonController(ComparisonControllers.StopWhenDifferent)
+                .build();
+        Assert.assertFalse(diff.toString(), diff.hasDifferences());
+    }
+
+    @Test
+    public void createRuleEffectFlippingMutantsTest() throws XPathExpressionException, ParsingException, ParserConfigurationException, SAXException, IOException {
+        for (String xpathString : xpathList) {
+            if (isRuleXpathString(xpathString)) {
+                NodeList nodes = (NodeList) xPath.evaluate(xpathString, doc.getDocumentElement(), XPathConstants.NODESET);
+                Assert.assertEquals(1, nodes.getLength());
+                Node node = nodes.item(0);
+                String effect = node.getAttributes().getNamedItem("Effect").getTextContent();
+                Assert.assertTrue(effect.equals("Permit") || effect.equals("Deny"));
+                List<Mutant> mutants = mutator.createRuleEffectFlippingMutants(xpathString);
+                Assert.assertEquals(1, mutants.size());
+                Mutant mutant = mutants.get(0);
+                Document newDoc = PolicyLoader.getDocument(IOUtils.toInputStream(mutant.encode(), Charset.defaultCharset()));
+                nodes = (NodeList) xPath.evaluate(xpathString, newDoc.getDocumentElement(), XPathConstants.NODESET);
+                Assert.assertEquals(1, nodes.getLength());
+                node = nodes.item(0);
+                String flippedEffect = node.getAttributes().getNamedItem("Effect").getTextContent();
+                Assert.assertTrue(flippedEffect.equals("Permit") || flippedEffect.equals("Deny"));
+                Assert.assertNotEquals(effect, flippedEffect);
+            }
+        }
+    }
+
+    @Test
+    public void createPolicyTargetTrueMutantsTest() throws XPathExpressionException, ParsingException, ParserConfigurationException, SAXException, IOException {
+        for (String xpathString : xpathList) {
+            if (isTargetXpathString(xpathString)) {
+//                System.out.println(xpathString);
+                NodeList nodes = (NodeList) xPath.evaluate(xpathString, doc.getDocumentElement(), XPathConstants.NODESET);
+                Assert.assertEquals(1, nodes.getLength());
+                Node node = nodes.item(0);
+//                System.out.println(XpathSolver.NodeToString(node, false, true));
+                List<Mutant> mutants = mutator.createPolicyTargetTrueMutants(xpathString);
+                if (!Mutator.isEmptyTarget(node)) {
+                    Assert.assertEquals(1, mutants.size());
+                    Mutant mutant = mutants.get(0);
+                    Document newDoc = PolicyLoader.getDocument(IOUtils.toInputStream(mutant.encode(), Charset.defaultCharset()));
+                    nodes = (NodeList) xPath.evaluate(xpathString, newDoc.getDocumentElement(), XPathConstants.NODESET);
+                    Assert.assertEquals(1, nodes.getLength());
+                    Node newNode = nodes.item(0);
+//                    System.out.println(XpathSolver.NodeToString(newNode, false, true));
+                    Assert.assertTrue(Mutator.isEmptyTarget(newNode));
+                } else {
+//                    System.out.println(XpathSolver.NodeToString(node, false, true));
+                    Assert.assertEquals(0, mutants.size());
+                }
+//                System.out.println("===========");
+            }
+        }
+    }
+}
